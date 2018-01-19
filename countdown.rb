6d87300134618b95d8f890056519f65f1c5fb8b9
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 02:17:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,5 +7,14 @@ def countdown(num)
 end
 sleep 5
 return "HAPPY NEW YEAR!"
+end
 
+def countdown_with_sleep(num)
+  x = 1
+  while x < num
+  puts "#{x} SECOND(S)!"
+  x += 1
+end
+sleep 5
+return "HAPPY NEW YEAR!"
 end
