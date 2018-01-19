c6c8de052e4a2147fa8758fbc7e9363f3c40b0ca
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 02:14:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 def countdown(num)
-  x = 1 
-  while x < num 
+  x = 1
+  while x < num
   puts "#{x} SECOND(S)!"
-  x += 1 
+  x += 1
 end
-return "HAPPY NEW YEAR!"
 sleep 5
-end
+return "HAPPY NEW YEAR!"
 
+end
